0946d1ac1ff42a03063a558150bd431ebfb89c05
Niels van der Pijl <nielsvanderpijl023@gmail.com>
2026-01-27 14:49:11 +0100

    Update main.py
    Uses secrets instead of random.
    Some other QoL changes.

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
-import random
+import secrets
 import string
 import time
 import pyperclip
 
-how_many_characters = int(input("How many characters do you want your password to have? (only type the number): "))
-how_many_passwords = int(input("How many passwords do you want? (only type the number): "))
-
-def generate_password():
-
+def generate_password(length):
     characters = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(random.choice(characters) for i in range(how_many_characters))
+    password = ''.join(secrets.choice(characters) for i in range(length))
     return password
 
+try:
+    how_many_characters = int(input("How many characters do you want your password to have? (numbers only): "))
+    how_many_passwords = int(input("How many passwords do you want? (numbers only): "))
+except ValueError:
+    print("Error: You must type a number.")
+    exit()
+
+saved_passwords = []
+
+print("\n--- Generated Passwords ---")
 
 for i in range(how_many_passwords):
-    password = generate_password()
+    password = generate_password(how_many_characters)
+    saved_passwords.append(password)
     print(f"Password: {password}")
-    pyperclip.copy(password)
-    print("Password copied to clipboard!")
+
+pyperclip.copy("\n".join(saved_passwords))
+print("\nAll passwords copied to clipboard!")
 
 print("Closing in:")
-for i in range(30, 0, -1):
-    print(i, end='\r')
+for i in range(10, 0, -1):
+    print(f"{i}...", end='\r')
     time.sleep(1)
